1d4f1cce291a5b1218069fbdd3e1f185cee636ac
Tommaso Negri <me@tommasongr.com>
2026-04-14 16:27:53 +0200

    add custom fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,21 @@
 	import favicon from "$lib/assets/favicon.svg";
 
 	import "$lib/styles/reset.css";
+	import "$lib/styles/tokens.css";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- Satoshi Font -->
+	<link rel="preconnect" href="https://api.fontshare.com/" />
+	<link href="https://api.fontshare.com/v2/css?f[]=satoshi@1,2&display=swap" rel="stylesheet" />
+
+	<!-- Inter Font -->
+	<link rel="preconnect" href="https://rsms.me/" />
+	<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 </svelte:head>
 
 {@render children()}
