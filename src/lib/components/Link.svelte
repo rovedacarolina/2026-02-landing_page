3bd0d7e2eb85b4eea91fd6d3a3ff09165cc08e2e
Tommaso Negri <me@tommasongr.com>
2026-04-14 18:00:00 +0200

    add Link component

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,47 @@
+<script>
+	import Icon from "./Icon.svelte";
+
+	const {
+		ref,
+		title = null,
+		children = null,
+		leadingIcon = null,
+		trailingIcon = null,
+	} = $props();
+</script>
+
+<a href={ref}>
+	{#if children}
+		{@render children()}
+	{:else}
+		{#if leadingIcon}
+			<Icon name={leadingIcon} />
+		{/if}
+
+		{title}
+
+		{#if trailingIcon}
+			<Icon name={trailingIcon} />
+		{/if}
+	{/if}
+</a>
+
+<style>
+	a {
+		@media screen and (prefers-color-scheme: light) {
+			--color-hover: var(--hex-brand-600);
+		}
+
+		@media screen and (prefers-color-scheme: dark) {
+			--color-hover: var(--hex-brand-500);
+		}
+
+		display: inline-flex;
+		align-items: center;
+		gap: var(--space-1);
+
+		&:hover {
+			color: var(--color-hover);
+		}
+	}
+</style>
