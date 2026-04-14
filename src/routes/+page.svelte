1887240893a99a1975aa127edb20914ae3cb8a8c
Tommaso Negri <me@tommasongr.com>
2026-04-14 18:33:20 +0200

    add hero to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="safe-area hero">
+	<h1>
+		This archive collects the best outcomes of the Web Design Laboratory in
+		Communication Design, Politecnico di Milano. The laboratory is directed by
+		Umberto Tolino, Christian Mazzoleni, Francesco di Gioia and Tommaso Negri
+		with love.
+	</h1>
+</section>
+
+<style>
+	.hero {
+		padding-block: var(--size-11);
+
+		h1 {
+			font-size: var(--size-7);
+			max-width: 35ch;
+		}
+	}
+</style>
